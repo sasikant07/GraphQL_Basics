fe6f8c99d5b9d42c432526079b7fdd8cb5e29689
Sasikant Biswal <biswalsashikant@gmail.com>
2024-05-01 02:03:01 +0530

    Added typedefs resolvers and schema

---
+++ games/index.js
@@ -0,0 +1,34 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+
+//db
+import db from "./db.js";
+
+//types
+import { typeDefs } from "./schema.js";
+
+const resolvers = {
+  Query: {
+    games() {
+      return db.games;
+    },
+    authors() {
+      return db.authors;
+    },
+    reviews() {
+      return db.reviews;
+    },
+  },
+};
+
+// server setup
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+});
+
+console.log(`Server ready at port ${url}`);
